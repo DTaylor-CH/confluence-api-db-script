fe4efe33f5e7ee51849657a0423c14f6cfec8f78
DTaylor-CH <dtaylor@companieshouse.gov.uk>
2022-01-05 17:06:11 +0000

    add new apply mongo tag subscript

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const readline = require("readline");
 
 // Internal imports:
 const { populateMongoSubScript } = require("./internal-scripts/populate-mongo");
+const {
+  applyMongoTagsSubScript,
+} = require("./internal-scripts/apply-mongo-tags");
 
 // Set-up to allow command line input to be assigned to variables, and program to exit when done
 const rl = readline.createInterface({
@@ -16,11 +19,14 @@ const prompt = (query) => new Promise((resolve) => rl.question(query, resolve));
 const executeChosenScript = async (scriptNumber) => {
   switch (scriptNumber) {
     case "0":
-      console.log("Script exiting");
+      console.log("Exiting...");
       rl.close();
       break;
     case "1":
       await populateMongoSubScript(prompt);
+      break;
+    case "2":
+      await applyMongoTagsSubScript(prompt);
       break;
     default:
       console.log("No script found with this number");
@@ -30,14 +36,15 @@ const executeChosenScript = async (scriptNumber) => {
 };
 
 const chooseAndExecuteChosenScript = async () => {
-  const scriptNumber = await prompt(
-    `
-Which script would you like to run?:
+  const scriptNumber = await prompt(`
+  -----------------------------------
+  Which script would you like to run? (enter a number):
+  -----------------------------------
   0. Exit
   1. Populate Mongo (Calls Confluence API for all pages in a single Confluence Space and saves metadata in a Mongo collection of your choice)
-    
-    `
-  );
+  2. Apply tags to Mongo (Calls Confluence API individually for each page in designated Mongo collection that is flagged as not-yet-updated)
+  -----------------------------------
+  `);
 
   if (scriptNumber) {
     executeChosenScript(scriptNumber);
---
+++ internal-scripts/apply-mongo-tags.js
@@ -0,0 +1,58 @@
+// Internal imports:
+const {
+  setDbParameters,
+  mongoConnect,
+  mongoDisconnect,
+  getAllMongoTaggedPagesNotTaggedInConfluence,
+} = require("../external-services/mongo-service");
+
+const {
+  setAuthorisationHeader,
+  setConfluenceSpace,
+} = require("../external-services/confluence-api");
+
+// User inputs own parametes for Mongo script or accepts env vars as default (by pressing 'enter')
+const applyMongoTagsSubScript = async (prompt) => {
+  const spaceName =
+    (await prompt(
+      `What is your Confluence Space name? (default=${process.env.SPACE_NAME}): `
+    )) || process.env.SPACE_NAME;
+
+  const mongoUri =
+    (await prompt(
+      `What is your MongoDB URI? (default=${process.env.MONGO_URI}): `
+    )) || process.env.MONGO_URI;
+
+  const dbName =
+    (await prompt(
+      `What is your Database name? (default=confluence_pages): `
+    )) || "confluence_pages";
+
+  const collectionName =
+    (await prompt(
+      `What is your Collection name? (default=${spaceName.toLowerCase()}_space): `
+    )) || spaceName.toLowerCase() + "_space";
+
+  const accessToken =
+    (await prompt(
+      `Whats is your Confluence API Access Token? (default=${process.env.ACCESS_TOKEN}): `
+    )) || process.env.ACCESS_TOKEN;
+
+  // guard against any undefined values before Mongo update script starts
+  if (mongoUri && dbName && collectionName && spaceName && accessToken) {
+    setDbParameters(mongoUri, dbName, collectionName);
+    setAuthorisationHeader(accessToken);
+    setConfluenceSpace(spaceName);
+    await mongoConnect();
+    getAllMongoTaggedPagesNotTaggedInConfluence();
+    await mongoDisconnect();
+  } else {
+    console.log(
+      "All values must be set, either by environment variables or by user input"
+    );
+  }
+};
+
+module.exports = {
+  applyMongoTagsSubScript,
+};
